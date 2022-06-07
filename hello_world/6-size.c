43650efa5bf27c1e6be18616a3b17f81ddf85518
majdi.developer <majdi.developer@gmail.com>
2022-06-07 09:38:36 +0100

    return size of data type

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)",sizeof(char));
-printf("Size of a char: %d byte(s)",sizeof(int));
-printf("Size of a char: %d byte(s)",sizeof(long int));
-printf("Size of a char: %d byte(s)",sizeof(long long int));
-printf("Size of a char: %d byte(s)",sizeof(float));
+printf("Size of a char: %d byte(s)", sizeof(char));
+printf("Size of a char: %d byte(s)", sizeof(int));
+printf("Size of a char: %d byte(s)", sizeof(long int));
+printf("Size of a char: %d byte(s)", sizeof(long long int));
+printf("Size of a char: %d byte(s)", sizeof(float));
 return (0);
 }
