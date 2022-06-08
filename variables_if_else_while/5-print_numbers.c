ce1001378f4a069d2676b8c10f6ad63725ae9bb6
majdi.developer <majdi.developer@gmail.com>
2022-06-08 15:55:40 +0100

    write numbers

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -13,13 +13,10 @@ int main(void)
 {
 	char a;
 
-	for (a = 'a'; a <= 'z'; ++a)
+	for (a = '0'; a <= '9'; ++a)
 	{
-		if (a != 'e' && a != 'q')
-		{
 		putchar(a);
-		}
-			}
+	}
 	putchar('\n');
 	return (0);
 
