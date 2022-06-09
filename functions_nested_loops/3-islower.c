751be7f89f1d7cd38b52a697c9ac3e921a6e451e
majdi.developer <majdi.developer@gmail.com>
2022-06-09 09:23:15 +0100

    check is c lowercase

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,18 +1,15 @@
-#include <unistd.h>
 #include "main.h"
 /**
- *_islower - Function write alphabet
+ *_islower - Function check number
  *@c: the number to verfiy
  *Return: 1 if lowercase
  *
  */
-
 int _islower(int c)
 {
 	if (c >= 65 && c <= 90)
 	{
 		return (1);
-
 	}
 	return (0);
 }
