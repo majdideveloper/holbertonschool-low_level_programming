f3779edacd5412841fdfc880642194ee0a385746
majdi.developer <majdi.developer@gmail.com>
2022-06-09 10:42:12 +0100

    return abs lsat digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_last_digit - Function return abs last digit
+ *@number: the number to verfiy
+ *Return: 0
+ *
+ */
+int print_last_digit(int number)
+{
+		number = number % 10;
+		return (number);
+}
