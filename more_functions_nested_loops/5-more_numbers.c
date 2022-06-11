0fae90a428fe7ec17fd338a98317cdefb45aa355
majdi.developer <majdi.developer@gmail.com>
2022-06-11 10:26:43 +0100

    print number from 0 - 14 10 times

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,17 @@ for (i = 0; i < 10; i++)
 {
 	for (j = 0; j <= 14; j++)
 	{
-	_putchar(j + '0');
 		if (j > 9)
 		{
 			_putchar('1');
+		}
+		if (j <= 9)
+		{
+			_putchar(j + '0');
+		}
+		else
+		{
+			_putchar((j - 9) + '0');
 		}
 	}
 _putchar('\n');
