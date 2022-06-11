d93c537aff57b270f6fdb3679a73496b8c2992e7
majdi.developer <majdi.developer@gmail.com>
2022-06-11 09:32:55 +0100

    print number from 0 - 14 10 times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *more_numbers - Function Display numbers from 0 - 9
+ */
+void more_numbers(void)
+{
+int i;
+int j;
+
+
+for (i = 0; i < 10; i++)
+{
+	for (j = 0; j <= 14; j++)
+	{
+		_putchar(j + '0');
+	}
+}
+_putchar('\n');
+}
