e4c6beb907651055367a026d03ac86e490613213
majdi.developer <majdi.developer@gmail.com>
2022-06-14 10:59:57 +0100

    print string rev

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *rev_string- Function
+ *@s: a pointer to char
+ *Return: Nothing
+ */
+void rev_string(char *s)
+{
+int i = 0;
+int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+_putchar('\n');
+}
