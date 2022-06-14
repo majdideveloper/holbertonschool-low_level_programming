93cd1b34186ac09aa757e59cfdfe827e0710409d
majdi.developer <majdi.developer@gmail.com>
2022-06-14 08:41:05 +0100

    print string

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_puts - Function
+ *@s: a pointer to char
+ *Return: Nothing
+ */
+void _puts(char *s)
+{
+
+	while (*s != '\0')
+	{
+		puts(s);
+		s++;
+	}
+}
