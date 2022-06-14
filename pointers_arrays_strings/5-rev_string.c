32e154b27f84747842ab10ae64caa74d51dfa08e
majdi.developer <majdi.developer@gmail.com>
2022-06-14 11:37:03 +0100

    print string rev

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  *rev_string- Function
  *@s: a pointer to char
@@ -11,7 +12,7 @@ void rev_string(char *s)
 	int j;
 	char c;
 
-	j = _strlen(s);
+	j = strlen(s);
 	for (i = 0, j -= 1; i < j; i++, j--)
 	{
 		c = s[i];
