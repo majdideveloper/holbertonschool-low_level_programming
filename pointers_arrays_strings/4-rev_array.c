870ba6135ab82129c68e9402995948b672c84896
majdi.developer <majdi.developer@gmail.com>
2022-06-16 09:47:57 +0100

    reverse array

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,11 @@
  */
 void reverse_array(int *a, int n)
 {
-int rev[n];
 int i;
+int rev[n];
 
 for (i = 0; i < n; i++)
 	rev[n - 1 - i] = a[i];
 for (i = 0; i < n; i++)
 	a[i] = rev[i];
-
-
-
-
 }
