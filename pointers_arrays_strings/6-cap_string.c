840a6854e9298e5ccec03d3765345c2e24f18882
majdi.developer <majdi.developer@gmail.com>
2022-06-16 11:45:23 +0100

    cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *cap_string- Function
+ *@a: pointer in the first item of array
+ *Return: char[]uppercase
+ */
+char *cap_string(char *a)
+{
+	int s, i;
+	char b;
+	int len = strlen(a);
+
+	for (i = 0; i < len; i++)
+	{
+		if (a[i - 1] == ' ')
+		{
+			if (a[i] >= 97 && a[i] <= 122)
+			{
+				s = a[i];
+				b = s - 32;
+				a[i] = b;
+
+			}
+
+
+		}
+	}
+	return (a);
+
+}
