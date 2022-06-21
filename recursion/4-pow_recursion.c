896e2d5db24b8b5134691cd93829503201399dc2
majdi.developer <majdi.developer@gmail.com>
2022-06-21 13:46:16 +0100

    power

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *_pow_recursion - Function return power r
+ *@x: int number
+ *@y: int number
+ *Return: int
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+
+
+
