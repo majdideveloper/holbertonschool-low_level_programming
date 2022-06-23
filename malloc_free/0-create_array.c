c94de1c5ce8d8b9100c00892e8a94d845e4b7ea1
majdi.developer <majdi.developer@gmail.com>
2022-06-23 17:25:27 +0100

    create array of char with malloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,7 +14,10 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
+
 	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		str[i] = c;
 	return (str);
