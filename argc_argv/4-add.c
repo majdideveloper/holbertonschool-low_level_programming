6ddf80610c81ac93c4e1abe97332aef40d185f0e
majdi.developer <majdi.developer@gmail.com>
2022-06-23 12:02:48 +0100

    display sum of list number  arg

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function
+ *@argc : number of arg
+ *@argv : list of String arg
+ *Return: int value
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 0; i < (argc - 1); i++)
+		{
+			if (atoi(argv[i + 1]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				sum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", sum);
+		return (0);
+	}
+}
