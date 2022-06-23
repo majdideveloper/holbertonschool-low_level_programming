57b86c82080ce20efa2cc355a88b02d45edc0e54
majdi.developer <majdi.developer@gmail.com>
2022-06-23 10:28:22 +0100

    number of argc - 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *@argc : number of arg
+ *@argv : list of String arg
+ *Return: int value
+ */
+int main(int argc, char *argv[])
+{
+	argc += 0;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
