29ced3363eb52533f7b64743385cb4dbc8bb5495
majdi.developer <majdi.developer@gmail.com>
2022-06-24 13:40:36 +0100

    array d2

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -12,30 +12,18 @@ int **alloc_grid(int width, int height)
 	int i, j;
 	int **arr = (int **) malloc(height * sizeof(int *));
 
-	if (arr == NULL)
-		return (NULL);
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
-
-
-
-
-
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		free(arr);
 		arr[i] = (int *)malloc(sizeof(int) * width);
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
 		}
 	}
-
-
-
-
-
 	return (arr);
 }
 
