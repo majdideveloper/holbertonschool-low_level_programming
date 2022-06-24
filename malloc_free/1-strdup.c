241f2b5b3f7a178a1ed01c6c6264c6af8c42815e
majdi.developer <majdi.developer@gmail.com>
2022-06-24 08:28:27 +0100

    function duplicated String

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *_strdup - fun create array
+ *@str : size of array
+ *Return: char
+ */
+char *_strdup(char *str)
+{
+unsigned long i;
+char *sen;
+
+	if (str == NULL)
+		return (NULL);
+
+
+	sen = malloc(sizeof(char) * (strlen(str) + 1));
+	for (i = 0; i < strlen(str) + 1; i++)
+		sen[i] = str[i];
+	return (sen);
+
+}
