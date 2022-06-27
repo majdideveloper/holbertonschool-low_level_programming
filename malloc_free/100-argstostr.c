8bee8487ed2ad81cc17ee38b82e9d8feaed442fb
majdi.developer <majdi.developer@gmail.com>
2022-06-27 09:09:16 +0100

    list of args

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *argstostr - fun list args
+ *@ac : int
+ *@av : pointer to pointer char
+ *Return: pointer to char "list of args"
+ */
+char *argstostr(int ac, char **av)
+{
+
+	int i;
+	char *newStr = (char *) malloc(ac * sizeof(char *));
+
+	if (ac == 0)
+		return (NULL);
+	if (av == NULL)
+		return (NULL);
+
+
+	if (newStr == NULL)
+		return (NULL);
+
+
+	for (i = 0; i < ac; i++)
+	{
+		printf("%s\n", av[i]);
+	}
+	return (newStr);
+}
