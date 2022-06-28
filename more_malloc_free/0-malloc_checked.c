b6c175c846e9e476f5f3582b3c684b6c25a16025
majdi aribi <majdi.developer@gmail.com>
2022-06-28 09:16:01 +0100

    malloc checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+
+#include <stdlib.h>
+#include <string.h>
+/**
+ *malloc_checked - fun list args
+ *@b : unsigned int
+ *Return : Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+b =  malloc(sizeof(b));
+return (b);
+}
