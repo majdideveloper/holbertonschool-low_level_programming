65ca81f6acf1b3826c8a2340bdbf25dd99618e82
majdi aribi <majdi.developer@gmail.com>
2022-06-28 18:53:13 +0100

    string nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < (strlen(s1)); i++)
 		newStr[i] = s1[i];
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 		newStr[strlen(s1) + i] = s2[i];
 
 	return (newStr);
