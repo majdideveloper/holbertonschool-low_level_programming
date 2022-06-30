0902f8edf7e24883ae260a7e4a884850adcf4302
majdi aribi <majdi.developer@gmail.com>
2022-06-30 16:06:28 +0100

    redifinition dog with typedef

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *new_dog - Fun
+ *@name: pointer to char
+ *@age: float
+ *@owner: pointer to char
+ *Return: dog_t
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+
+	if (!d)
+		return (NULL);
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	return (d);
+}
