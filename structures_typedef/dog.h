ede3ac0ec136380b8e5a93fe66713c92f583abed
majdi aribi <majdi.developer@gmail.com>
2022-06-30 10:46:21 +0100

    struct Dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,15 @@
+/**
+ * struct dog - dog
+ * @name: name dog
+ * @age: age dog
+ * @owner: owner of the dog
+ *
+ * Description: struct of dog
+ */
+struct dog
+{
+
+	char* name;
+	float age;
+	char* owner;
+};
