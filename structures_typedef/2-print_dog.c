4eb1602f7bd242f4ed200de4f8505b721a8c75a1
majdi aribi <majdi.developer@gmail.com>
2022-06-30 14:58:51 +0100

    print dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *print_dog - Function init value of struct dog
+ *@d: pointer to struct dog
+ *Return: Nothing
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf("");
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+	printf("Name: %s\n", d->name);
+	if (d->age == 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %lf\n",d->age);
+	printf("Owner: %s\n", d->owner);
+
+
+		
+	
+}
