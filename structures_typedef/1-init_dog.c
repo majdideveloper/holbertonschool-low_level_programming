9d0d1e358e6448e0f65b8a5c770c6197a1f81dcd
majdi aribi <majdi.developer@gmail.com>
2022-06-30 11:12:35 +0100

    init struct dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "dog.h"
+/**
+ *init_dog - Function init value of struct dog
+ *@d: pointer to struct dog
+ *@name: pointer to char 
+ *@age: float 
+ *@owner: pointer to char 
+ *Return: Nothing
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
