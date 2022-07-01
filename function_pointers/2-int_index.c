ed3920d052e9ded0a641e0850eea54cb1f0b4cbd
majdi aribi <majdi.developer@gmail.com>
2022-07-01 23:42:31 +0100

    pointer to function print number of item in list

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ *int_index - Fun
+ *@array: pointer to int
+ *@size: int
+ *@cmp: function
+ *Return: 0
+ *
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	else
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]))
+				return (i);
+			
+	return (-1);
+}
