307a15667066327711062570a4ded33613d186d8
majdi aribi <majdi.developer@gmail.com>
2022-07-01 22:11:05 +0100

    pointer to function print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *print_name - Fun
+ *@name: pointer to char name
+ *@f: function
+ *Return: 0
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+  
+    f(name);  
+}
