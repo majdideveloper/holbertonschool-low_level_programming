fadfb5de6304442e4bc678fffdf05179b14dac12
majdi aribi <majdi.developer@gmail.com>
2022-07-05 13:05:18 +0100

    print Numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *sum_them_all - FUN return sum all argument
+ *@n: const unsigned int
+ *Return : sum int
+ *
+ */
+void print_numbers(const char *separator,const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+
+	va_start (ap, n);         /* Initialize the argument list. */
+
+
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg (ap, int));
+if (separator != NULL && i != (n - 1))
+
+		printf("%s", separator);
+
+	}
+		printf("\n");
+	va_end (ap);                  /* Clean up. */
+}
+
+
