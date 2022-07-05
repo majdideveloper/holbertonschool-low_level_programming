c6bdcd913de2d208c01d0a4f3eee54b8e7811ddc
majdi aribi <majdi.developer@gmail.com>
2022-07-05 13:13:19 +0100

    print Numbers

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -7,27 +7,27 @@
  *Return : sum int
  *
  */
-void print_numbers(const char *separator,const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
 
 
-	va_start (ap, n);         /* Initialize the argument list. */
+	va_start(ap, n);         /* Initialize the argument list. */
 
 
 
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg (ap, int));
-if (separator != NULL && i != (n - 1))
+		printf("%d", va_arg(ap, int));
+		if ((separator != NULL) && (i != (n - 1)))
 
-		printf("%s", separator);
+			printf("%s", separator);
 
 	}
-		printf("\n");
-	va_end (ap);                  /* Clean up. */
+	printf("\n");
+	va_end(ap);                  /* Clean up. */
 }
 
 
