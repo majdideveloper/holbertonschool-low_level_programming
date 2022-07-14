e5749b83aaf7e8dc11306d3492fd8482cfab60b8
majdi aribi <majdi.developer@gmail.com>
2022-07-14 16:28:14 +0100

    print list with len of string

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,6 +11,7 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
+	
 
 	while (h != NULL)
 	{
@@ -22,7 +23,7 @@ size_t print_list(const list_t *h)
 			++n;
 			continue;
 		}
-
+		
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		++n;
---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *add_node - add other item to  linked list
+ *@head: pointer to linked list
+ *@str: pointer to string to add
+ * Return: list_t new linked-list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+struct list *newItem;
+ newItem = (struct list*)malloc(sizeof(struct list));
+ if ( newItem == NULL )
+	 return (NULL);
+  
+   newItem->str = strdup(str); 
+   newItem->len = strlen(str);
+   newItem->next = *head;
+   *head = newItem;
+return (*head);
+}
+
+
+
