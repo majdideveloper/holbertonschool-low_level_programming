b1b436a717eb55094d311e1298afe6c6523fdad7
majdi aribi <majdi.developer@gmail.com>
2022-07-14 21:14:25 +0100

    print list of int

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_listint - print a list of linked list
+ * @h: pointer to linked list
+ *
+ * Return: size_t
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+
+	while (h != NULL)
+	{
+
+		
+
+		printf("%d\n", h->n);
+		h = h->next;
+		++n;
+	}
+	return (n);
+}
+
+
---
+++ more_singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+## holbertonschool-low_level_programming
+# more_singly_linked_lists
