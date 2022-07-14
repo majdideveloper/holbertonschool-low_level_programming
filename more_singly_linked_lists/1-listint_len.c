74c6f39f052c2c6c238a93b5bec37d89f38f3e21
majdi aribi <majdi.developer@gmail.com>
2022-07-14 21:30:12 +0100

    print element len of list

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_listint - print a list of linked list
+ * @h: pointer to linked list
+ *
+ * Return: size_t
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+
+	while (h != NULL)
+	{
+		h = h->next;
+		++n;
+	}
+	return (n);
+}
+
+
