1f1c6ac29bd2e7f4f513c2b1a9329641c54c5b76
majdi aribi <majdi.developer@gmail.com>
2022-07-14 22:20:25 +0100

    print element  of list int

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -3,10 +3,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 /**
- *add_nodeint_end - add other item to  linked list
+ *free_listint - free  linked list
  *@head: pointer to linked list
- *@n: int to add
- * Return: list_t new linked-list
+ *Return: list_t new linked-list
  */
 void free_listint(listint_t *head)
 {
