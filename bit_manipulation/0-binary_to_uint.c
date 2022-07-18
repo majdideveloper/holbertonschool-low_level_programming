c9526f03ae3e20f482e4d22b82f37aed7fa4e889
majdi aribi <majdi.developer@gmail.com>
2022-07-18 16:08:43 +0100

    binary to int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,21 +11,25 @@
 unsigned int binary_to_uint(const char *b)
 {
 
-	unsigned int bin,dec = 0;
+	unsigned int bin,dec = 0, base = 1, rem;  
 	int i, len;
 
 	if (b == NULL)
 		return (0);
 	len = strlen(b);
 	for (i = 0; i < len; i++){
-		if (b[i] != '0' && b[i] != '1')
-			return (0);	
+	    if (b[i] != '0' && b[i] != '1'   )
+			return (0);
 	}
 	bin = atoi(b);
-	for (i = 0;bin;i++, bin /= 10)
-	{
-		if(bin % 10)
-			dec += pow(2, i);
-	}
+	 while ( bin > 0)  
+    {  
+        rem = bin % 10; /* divide the binary number by 10 and store the remainder in rem variable. */  
+        dec = dec + rem * base;  
+        bin= bin / 10; // divide the number with quotient  
+        base = base * 2;  
+    }  
+	
+	
 	return (dec);
 }
