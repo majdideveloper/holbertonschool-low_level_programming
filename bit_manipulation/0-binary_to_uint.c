92eecd1d0fe7e6a78ed006ea61700f8b930baf7b
majdi aribi <majdi.developer@gmail.com>
2022-07-18 15:36:45 +0100

    binary to int

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,15 +10,22 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int bin, dec = 0;
-	unsigned int i;
 
+	unsigned int bin,dec = 0;
+	int i, len;
+
+	if (b == NULL)
+		return (0);
+	len = strlen(b);
+	for (i = 0; i < len; i++){
+		if (*b != '0' && *b != '1'   )
+			return (0);	
+	}
 	bin = atoi(b);
-	for(i = 0;bin;i++, bin /= 10)
+	for (i = 0;bin;i++, bin /= 10)
 	{
-		if (bin % 10)
+		if(bin % 10)
 			dec += pow(2, i);
 	}
-
 	return (dec);
 }
