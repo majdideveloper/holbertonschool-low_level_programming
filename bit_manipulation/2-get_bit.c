617932f4bc3db3d20055ea2f114187306ab05001
majdi aribi <majdi.developer@gmail.com>
2022-07-19 16:03:02 +0100

    set bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * get_bit - function that prints the bit of index
- * @n: the number 
- * @index: index of bit 
+ * @n: the number
+ * @index: index of bit
  * Return: void
  */
 int get_bit(unsigned long int n, unsigned int index)
