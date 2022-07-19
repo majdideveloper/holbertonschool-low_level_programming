42c71b70e55e1839c93b676fa77221c43b76c4c8
majdi aribi <majdi.developer@gmail.com>
2022-07-19 16:01:48 +0100

    set bit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,11 +1,14 @@
 #include "main.h"
 /**
- * print_binary - function that prints the binary reprisentation of a number
- * @n: the number to print
+ * get_bit - function that prints the bit of index
+ * @n: the number 
+ * @index: index of bit 
  * Return: void
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-return (n >> index) & 0x01;
+	if (index >= sizeof(int) * 8)
+		return (-1);
+	return ((n >> index) & 0x01);
 }
 
