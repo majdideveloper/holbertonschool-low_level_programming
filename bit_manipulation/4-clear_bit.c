4c89b1534fb35f5d035c64573c02e83372239d1b
majdi aribi <majdi.developer@gmail.com>
2022-07-19 16:27:10 +0100

    clear bit

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * set_bit - function that change the bit of index
+ * @n: the number
+ * @index: index of bit
+ * Return: void
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(int) * 8)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
+}
+
