2d48e0082389c940f4e1561792054f82e9bd9b6d
majdi aribi <majdi.developer@gmail.com>
2022-07-25 22:09:20 +0100

    copy string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -3,14 +3,15 @@
 #include <string.h>
 /**
  *_strcpy - Function
- *@dest: a pointer to char destination 
+ *@dest: a pointer to char destination
  *@src: a pointer to char source
  *Return: pointer to char dest
  */
 char *_strcpy(char *dest, char *src)
 
 {
-	int i ;
+	int i;
+
 	for (i = 0;  src[i] != '\0'; i++)
 		dest[i] = src[i];
 
