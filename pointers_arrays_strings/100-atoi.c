a564ac26c40a554cc49035cc46f22f9747e2da7b
majdi aribi <majdi.developer@gmail.com>
2022-07-27 07:58:04 +0100

    atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,7 +11,7 @@
 
 int _atoi(char *s)
 {
-	int number = 0;
+unsigned int number = 0;
 	int sign = 1;
 	int i;
 	for (i = 0;s[i] != '\0';i++){
@@ -19,10 +19,12 @@ int _atoi(char *s)
 		{
 			sign *= (-1);
 		}
-		if (s[i] >= '0' && s[i]<= '9'){
+		else if (s[i] >= '0' && s[i]<= '9'){
 			number = number * 10 + s[i] - '0';
 		}
-	}
+		else if (number > 0 )
+			break;
+		}
 	return (number * sign);
 }	
 
