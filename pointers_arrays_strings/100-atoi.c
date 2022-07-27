b266bb0af1746920c4a5abd35f52e0170d04aa0e
majdi aribi <majdi.developer@gmail.com>
2022-07-27 08:06:13 +0100

    atoi function

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *_strcpy - Function
- *@dest: a pointer to char destination
- *@src: a pointer to char source
- *Return: pointer to char dest
+ *_atoi - Function convert string to int
+ *@s: a pointer to char string
+ *Return: int 
  */
 
 
@@ -14,14 +13,12 @@ int _atoi(char *s)
 unsigned int number = 0;
 	int sign = 1;
 	int i;
-	for (i = 0;s[i] != '\0';i++){
-		if (s[i]== '-')
-		{
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
 			sign *= (-1);
-		}
-		else if (s[i] >= '0' && s[i]<= '9'){
+		else if (s[i] >= '0' && s[i] <= '9')
 			number = number * 10 + s[i] - '0';
-		}
 		else if (number > 0 )
 			break;
 		}
