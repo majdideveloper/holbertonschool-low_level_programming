1b5587af5c530a07c9ee937eb2342ab5e9e2d345
majdi aribi <majdi.developer@gmail.com>
2022-08-09 16:23:26 +0100

    leet codage

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,23 +3,23 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- *leet - Function coding string 
+ *leet - Function coding string
  *@s: a pointer to char string
  *Return: pointer to char
  */
 char *leet(char *s)
 {
-	char arrayCodage [10][3] = {"a=4", "A=4", "e=3",
+	char arrayCodage [10] [3] = {"a=4", "A=4", "e=3",
 		"E=3", "o=0", "O=0", "t=7", "T=7", "l=1", "L=1"};
 	int i, j, lenStr = strlen(s);
-	char *newString = (char*)malloc(lenStr * sizeof(char)); 
+	char *newString = (char *)malloc(lenStr * sizeof(char));
 
-	for( i = 0; i < lenStr; i++)
+	for ( i = 0; i < lenStr; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == arrayCodage[j][0])
-				s[i] = arrayCodage[j][2]; 
+				s[i] = arrayCodage[j][2];
 		}
 	}
 	newString = s;
