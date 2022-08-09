769778dc37901e31a595775dfa5f2bcea638f995
majdi aribi <majdi.developer@gmail.com>
2022-08-09 16:25:08 +0100

    leet codage

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,12 +9,12 @@
  */
 char *leet(char *s)
 {
-	char arrayCodage [10] [3] = {"a=4", "A=4", "e=3",
+	char arrayCodage[10][3] = {"a=4", "A=4", "e=3",
 		"E=3", "o=0", "O=0", "t=7", "T=7", "l=1", "L=1"};
 	int i, j, lenStr = strlen(s);
 	char *newString = (char *)malloc(lenStr * sizeof(char));
 
-	for ( i = 0; i < lenStr; i++)
+	for (i = 0; i < lenStr; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
