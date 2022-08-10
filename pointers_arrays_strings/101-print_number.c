0350314eb098b1da93a7ef31d8de5c2720e312f0
majdi aribi <majdi.developer@gmail.com>
2022-08-10 07:49:19 +0100

    print number with just putchar

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- *_strlen - Function
- *@s: a pointer to char
- *Return: number of char
+ *print_number - Function print number with putchar
+ *@n: int number to  print
+ *Return: Nothing
  */
 void print_number(int n)
 {
-	 if (n < 0) {
-        _putchar('-');
-        n = -n;
-    }
-    if (n / 10 != 0)
-        print_number(n / 10);
-   _putchar((n % 10) + '0');
-
-
-
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
 }
