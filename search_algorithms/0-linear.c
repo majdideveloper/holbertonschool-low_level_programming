3b5adc84054559d99be242364aa8541ad77633f6
majdi aribi <majdi.developer@gmail.com>
2022-08-27 08:20:31 +0100

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,7 +11,7 @@ int linear_search(int *array, size_t size, int value)
 	size_t i, indexValue = 0;
 
 	if (!array)
-		return -1;
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 			break;
 		}
 	}
-	if (indexValue == 0) 
+	if (indexValue == 0)
 		indexValue = -1;
 	return (indexValue);
 }
