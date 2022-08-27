e8cb17b13ae336a8cd9e3486102797df1d554dc7
majdi aribi <majdi.developer@gmail.com>
2022-08-27 08:15:40 +0100

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,9 +1,11 @@
 #include "search_algos.h" 
-
-
-
-
-
+/**
+ *linear_search - function search value in array and return index this value
+ *@array : pointer  to array to search in
+ *@size : size of array
+ *@value : value to find in array
+ *Return: int index of value if found else -1
+ */
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i, indexValue = 0 ;
@@ -12,7 +14,7 @@ int linear_search(int *array, size_t size, int value)
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i]==value)
+		if (array[i] == value)
 		{
 			indexValue = i;
 			break;
