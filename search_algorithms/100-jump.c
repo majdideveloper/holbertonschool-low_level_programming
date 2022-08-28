0bd0fed4c21ec05e7b4112062ef0b29649076e9b
majdi aribi <majdi.developer@gmail.com>
2022-08-28 16:22:44 +0100

    jumpppp

--- search_algorithms/100-jump.c
+++ search_algorithms/100-jump.c
@@ -1,10 +1,11 @@
 #include "search_algos.h"
 /**
- *linear_search - function search value in array and return index this value
+ *jump_search - function search value in array and return index this value
  *@array : pointer  to array to search in
  *@size : size of array
  *@value : value to find in array
- *Return: int index of value if found else -1
+ *Return: int index of value if  found   
+ *else -1
  */
 int jump_search(int *array, size_t size, int value)
 {
@@ -21,20 +22,17 @@ int jump_search(int *array, size_t size, int value)
 	{
 		startIndex += step;
 		endIndex += step;
-		printf("Value checked array[%lu] = [%d]\n", startIndex,array[startIndex]);
+		printf("Value checked array[%lu] = [%d]\n", startIndex, array[startIndex]);
 
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", startIndex,endIndex);
+	printf("Value found between indexes [%lu] and [%lu]\n", startIndex, endIndex);
 	for (i = startIndex; i < endIndex; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", i,array[i]);
-		if(i == size - 1)
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (i == size - 1)
 			break;
-		if(value == array[i])
-		{
+		if (value == array[i])
 			return (i);
-		}
-
 	}
 	return (-1);
 }
