e88974bdfc42940d732ecd9cd1045acf10611da6
majdi aribi <majdi.developer@gmail.com>
2022-08-28 16:11:41 +0100

    jumpppp

---
+++ search_algorithms/100-jump.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+/**
+ *linear_search - function search value in array and return index this value
+ *@array : pointer  to array to search in
+ *@size : size of array
+ *@value : value to find in array
+ *Return: int index of value if found else -1
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	int  step; 
+	size_t	i, startIndex, endIndex, val;
+
+	step = sqrt(size);
+	startIndex = 0;
+	endIndex = step;
+	i = 0; val = 0;
+
+	printf("Value checked array[%lu] = [%d]\n", startIndex,array[startIndex]);
+	while(value > array[endIndex] && size > endIndex)
+	{
+		startIndex += step;
+		endIndex += step;
+		printf("Value checked array[%lu] = [%d]\n", startIndex,array[startIndex]);
+
+	}
+	printf("Value found between indexes [%lu] and [%lu]\n", startIndex,endIndex);
+	for(i = startIndex; i < endIndex; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i,array[i]);
+		if(i == size - 1)
+			break;
+		if(value == array[i])
+		{
+			return (i);
+		}
+
+	}
+	return (-1);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,10 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array (int *array, int firstItem , int lastItem);
-
+int jump_search(int *array, size_t size, int value);
 #endif
 
