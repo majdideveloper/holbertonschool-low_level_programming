11c7f0a85f27182362374b421e2235ed727ff6d9
majdi aribi <majdi.developer@gmail.com>
2022-08-29 12:43:41 +0100

    :zap: find the first index of value with binary search :zap:

--- search_algorithms/104-advanced_binary.c
+++ search_algorithms/104-advanced_binary.c
@@ -1,5 +1,32 @@
 #include "search_algos.h"
 /**
+ *binary_search_recursive - function search value in array and return index this value
+ *@array : pointer  to array to search in
+ *@low : size of array
+ *@high : size of array
+ *@value : value to find in array
+ *Return: int index of value if found else -1
+ */
+int search_binary_recursive(int *array, size_t low, size_t high, int value)
+{
+
+	if(low > high)
+		return(-1);
+	print_array(array, low, high);
+	size_t middle;
+
+	middle = low + (high - low) / 2;
+	if (array[middle] == value &&(middle == low || array[middle - 1] != value))
+		return middle;
+	if(array[middle] >= value)
+		return (search_binary_recursive(array, low , middle,value));
+	return (search_binary_recursive(array, middle + 1, high, value));
+
+
+
+
+}
+/**
  *binary_search - function search value in array and return index this value
  *@array : pointer  to array to search in
  *@size : size of array
@@ -8,53 +35,15 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t  indexValue = 0;
-	int startIndex, endIndex, middle;
-
-	if (!array)
+	size_t startIndex, endIndex;
+	
+	if(!array || size == 0)
 		return (-1);
+
 	startIndex = 0;
 	endIndex = size - 1;
-	while (startIndex <= endIndex)
-	{
-		middle = startIndex + (endIndex - startIndex) / 2;
-		print_array(array, startIndex, endIndex);
-		if (array[middle] == value)
-		{
-			indexValue = middle;
-			endIndex = middle - 1;
+	return (search_binary_recursive(array, startIndex, endIndex, value));
 
-		}
-		else if (value < array[middle])
-			endIndex = middle - 1;
-		else
-			startIndex = middle + 1;
 
-	}
-	if (indexValue == 0)
-		indexValue = -1;
-	return (indexValue);
-}
-/**
- *print_array - function print all item of list from firstItem to lastItem
- *@array : pointer  to array
- *@firstItem : index of first item to print
- *@lastItem : index of last item  to print
- *Return: Nothing
- */
-void print_array(int *array, int firstItem, int lastItem)
-{
-	int i;
-
-	printf("Searching in array: ");
-	for (i = firstItem; i <= lastItem; i++)
-	{
-		if (i == lastItem)
-		{
-			printf("%d\n", array[i]);
-			break;
-		}
-		printf("%d, ", array[i]);
-	}
 
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -12,4 +12,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int search_binary_recursive(int *array, size_t low, size_t high, int value);
 #endif
