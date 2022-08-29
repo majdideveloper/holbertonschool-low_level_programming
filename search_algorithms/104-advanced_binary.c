3c4898575acca17b09405d44122ee2f957514a29
majdi aribi <majdi.developer@gmail.com>
2022-08-29 12:56:02 +0100

    :zap: find the first index of value with binary search :zap:

--- search_algorithms/104-advanced_binary.c
+++ search_algorithms/104-advanced_binary.c
@@ -1,9 +1,9 @@
 #include "search_algos.h"
 /**
- *binary_search_recursive - function search value in array and return index this value
+ *search_binary_recursive - function search value in array and return index this value
  *@array : pointer  to array to search in
- *@low : size of array
- *@high : size of array
+ *@low : low element in array
+ *@high : high element in array
  *@value : value to find in array
  *Return: int index of value if found else -1
  */
@@ -11,10 +11,8 @@ int search_binary_recursive(int *array, size_t low, size_t high, int value)
 {
 	size_t middle;
 
-
 	if(low > high)
 		return(-1);
-/*	print_array(array, low, high);*/
 	
 	printf("Searching in array: ");
 	for (middle = low;	middle < high; middle++)
@@ -24,16 +22,14 @@ int search_binary_recursive(int *array, size_t low, size_t high, int value)
 	middle = low + (high - low) / 2;
 	if (array[middle] == value &&(middle == low || array[middle - 1] != value))
 		return middle;
+
 	if(array[middle] >= value)
 		return (search_binary_recursive(array, low , middle,value));
+
 	return (search_binary_recursive(array, middle + 1, high, value));
-
-
-
-
 }
 /**
- *binary_search - function search value in array and return index this value
+ *advanced_binary - function search value in array and return index this value
  *@array : pointer  to array to search in
  *@size : size of array
  *@value : value to find in array
@@ -41,13 +37,8 @@ int search_binary_recursive(int *array, size_t low, size_t high, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-
-
 	if(!array || size == 0)
 		return (-1);
 
 	return (search_binary_recursive(array, 0, size - 1, value));
-
-
-
 }
