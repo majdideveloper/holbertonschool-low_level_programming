f90aa69a992447579a56a2f106e53d289fda8af6
majdi aribi <majdi.developer@gmail.com>
2022-08-29 12:52:14 +0100

    :zap: find the first index of value with binary search :zap:

--- search_algorithms/104-advanced_binary.c
+++ search_algorithms/104-advanced_binary.c
@@ -9,13 +9,18 @@
  */
 int search_binary_recursive(int *array, size_t low, size_t high, int value)
 {
-int middle;
+	size_t middle;
 
 
 	if(low > high)
 		return(-1);
-	print_array(array, low, high);
+/*	print_array(array, low, high);*/
 	
+	printf("Searching in array: ");
+	for (middle = low;	middle < high; middle++)
+		printf("%d, ", array[middle]);
+	printf("%d\n", array[middle]);
+
 	middle = low + (high - low) / 2;
 	if (array[middle] == value &&(middle == low || array[middle - 1] != value))
 		return middle;
@@ -36,14 +41,12 @@ int middle;
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t startIndex, endIndex;
-	
+
+
 	if(!array || size == 0)
 		return (-1);
 
-	startIndex = 0;
-	endIndex = size - 1;
-	return (search_binary_recursive(array, startIndex, endIndex, value));
+	return (search_binary_recursive(array, 0, size - 1, value));
 
 
 
