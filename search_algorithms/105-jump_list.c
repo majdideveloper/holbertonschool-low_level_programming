0357a4fce31d1ca310027a355b154e0a05fc261c
majdi aribi <majdi.developer@gmail.com>
2022-08-30 13:35:34 +0100

    :rocket: search in linked list:rocket:

---
+++ search_algorithms/105-jump_list.c
@@ -0,0 +1,67 @@
+#include "search_algos.h"
+/**
+ *jump_search - function search value in array and return index this value
+ *@array : pointer  to array to search in
+ *@size : size of array
+ *@value : value to find in array
+ *Return: int index of value if  found
+ *else -1
+ */
+listint_t *nextStep (listint_t *head,int step);
+
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	
+	int step;
+	listint_t *startIndex, *endIndex;
+
+	if (!list )
+		return (NULL);
+
+
+	step = sqrt(size);
+	startIndex = list;
+
+
+	endIndex = nextStep(list,step);
+	/*	printf("%d\n", endIndex->n);*/
+		
+	while ((value > endIndex->n && size > endIndex->index) || !endIndex)
+	{	
+		startIndex = endIndex ;
+		endIndex = nextStep(endIndex,step);
+	printf("Value checked at index kk [%lu] = [%d]\n", startIndex->index, startIndex->n);
+
+	}
+	printf("Value checked at index [%lu] = [%d]\n", endIndex->index, endIndex->n);
+	printf("Value found between indexes [%lu] and [%lu]\n", startIndex->index, endIndex->index);
+		while (startIndex->next != NULL)
+	{
+		
+	printf("Value checked at index [%lu] = [%d]\n", startIndex->index, startIndex->n);
+		if (startIndex->n == value)
+			return (startIndex);
+		startIndex = startIndex->next;
+		
+	}
+	return (NULL);
+
+}
+
+
+listint_t *nextStep (listint_t *head,int step)
+{
+	int i = 0;
+	while(head->next != NULL)
+	{head = head->next;
+		i++;
+		if (i == step)
+		{
+
+			break;
+		}
+
+
+	}
+	return (head);
+}
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,6 +4,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+/**
+ * struct listint_s - singly linked list
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+    int n;
+    size_t index;
+    struct listint_s *next;
+} listint_t;
+
+
+
+
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
@@ -13,4 +32,13 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 int search_binary_recursive(int *array, size_t low, size_t high, int value);
+void free_list(listint_t *list);
+listint_t *create_list(int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+
+
+
+
 #endif
