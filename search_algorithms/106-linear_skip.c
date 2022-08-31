93f0f37bd0e23133f4ee26524c701bd34ba3ddf9
majdi aribi <majdi.developer@gmail.com>
2022-08-31 08:20:12 +0100

    :art: linear skip function :art:

--- search_algorithms/106-linear_skip.c
+++ search_algorithms/106-linear_skip.c
@@ -1,34 +1,22 @@
 #include "search_algos.h"
 /**
- *jump_list - function search value in linked list and return index this value
+ *linear_skip - function search value in linked list and return index this value
  *@list : pointer  to linked list to search in
- *@size : size of array
  *@value : value to find in array
- *Return: int index of value if found else -1
+ *Return: int index of value if found else NULL
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-
 	skiplist_t *startIndex, *endIndex;
-
 
 	if (!list)
 		return (NULL);
 
-
-
 	startIndex = list;
 	endIndex = list->express;
 
-
-
-
-
-
-
-	while (value > endIndex->n 
-
-			&& endIndex-> express != NULL)
+	while ((value > endIndex->n && endIndex-> express != NULL)
+		 || endIndex->next == NULL)
 	{
 		startIndex = endIndex;
 		endIndex = endIndex->express;
@@ -38,7 +26,6 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 	}
 	printf("Value checked at index [%lu] = [%d]\n",
 			endIndex->index, endIndex->n);
-
 	printf("Value found between indexes [%lu] and [%lu]\n",
 			startIndex->index, endIndex->index);
 	while (startIndex)
