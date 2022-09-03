7297b81bf3ca792c42654e4ec472c8457a3c1024
majdi aribi <majdi.developer@gmail.com>
2022-09-03 17:24:08 +0100

    return sum of linked list

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ *sum_listint - calcul sum of item linked list
+ *@head: pointer to linked list
+ * Return: int sum 
+ */
+	int sum_listint(listint_t *head)
+{
+	listint_t *temp = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+		sum += temp->n;
+		
+	}
+	return (sum);
+}
+
